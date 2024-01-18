4fb28d4ff1fdc391e1d19f662ec76b7596a56543
dhiraj <dhirajpk28@gmail.com>
2024-01-18 12:55:03 +0530

    swicth canse

---
+++ Codintionals with SwitchnCase/Codintionals with SwitchnCase/Source.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	//int Rating = 3;
+
+	//switch (Rating) {
+	//case 1:
+	//	puts("Your rating is 1\n");
+	//	break;
+	//case 2:
+	//	puts("Your rating is 2\n");
+	//	break;
+	//case 3:
+	//	puts("Your rating is 3\n");
+	//	break;
+	//case 4:
+	//	puts("Your rating is 4\n");
+	//	break;
+	//case 5:
+	//	puts("Your rating is 5\n");
+	//	break;
+	//default:
+	//	puts("Plase enter Rating from 1 to 5 only");
+	//	break;
+	//}return 0;
+
+	/* Custom Assignt for user review*/
+
+	//int Rating;
+	//puts("Give Us your Rating for our servies between 1 to 5");
+
+	//cin >> Rating;
+
+
+	//switch (Rating) {
+	//case 1:
+	//	puts("Your rating is 1\n");
+	//	break;
+	//case 2:
+	//	puts("Your rating is 2\n");
+	//	break;
+	//case 3:
+	//	puts("Your rating is 3\n");
+	//	break;
+	//case 4:
+	//	puts("Your rating is 4\n");
+	//	break;
+	//case 5:
+	//	puts("Your rating is 5\n");
+	//	break;
+	//default:
+	//	puts("Plase enter Rating from 1 to 5 only");
+	//	break;
+	//}return 0;
+
+	char Names;
+	cin >> Names;
+	/*this tell us it only works on intgrial value only*/
+
+	/*switch (Names) {
+		case 'a':
+		cout << "OP your name srat with A\n";
+		break;
+		default:
+			cout << "That wirte you can Change it\n";
+	}*/
+}
