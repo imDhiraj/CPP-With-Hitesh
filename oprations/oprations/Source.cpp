6e80fe655d135df5ff69953a168c0fac5d482bf4
dhiraj <dhirajpk28@gmail.com>
2024-02-14 14:46:02 +0530

    most used type of operation in cpp are written but there has to be some specfic use in that so that we can use it 1. combine oprition for storeing the value with  less space uses (ex.+=)

---
+++ oprations/oprations/Source.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//int  points = 4;
+	//int life = 4;
+	//int score = life + points;
+	//int score1 = life - points;
+	//int score2 = life * points;
+	//int score3 = life / points;
+
+	//cout << score << endl << score1 << endl << score2 << endl << score3 << endl;
+
+	//int score4 = life % points;
+
+	//cout<< score4 << endl;
+
+	//if (life != 0) {
+	//	cout << "not equal to is working" << endl;
+
+	//}
+
+	//score = score + points;
+	//cout << score << endl;
+
+	//cout << "other tecquice to use optater\n";
+
+	///* score += points;
+	// cout << score << endl;*/
+	///* score -= points;
+	// cout << score << endl;*/
+	///* score *= points;
+	// cout << score << endl;*/
+	///* score /=  points;
+
+	// cout << score << endl;
+	//*/
+
+
+	int a = 2;
+	int b = 3;
+	float c = a + b;
+	cout << c << endl;
+	c /= a;
+	cout << c << endl;
+
+}
