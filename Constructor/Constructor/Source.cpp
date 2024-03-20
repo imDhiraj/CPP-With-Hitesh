98dcdf69e398a035e28f6f4718ebaca1d4c43d0e
dhiraj <dhirajpk28@gmail.com>
2024-03-20 23:40:54 +0530

    construture use and rewrite

---
+++ Constructor/Constructor/Source.cpp
@@ -0,0 +1,34 @@
+// Read This for better understanding it about Three rule of constructer https://stackoverflow.com/questions/4172722/what-is-the-rule-of-three
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Phone {
+	string _Name = "";
+	string _OS = "";
+	int _price = 0;
+
+public:
+	Phone();//construtor
+	string getos()const{return _OS;}
+	Phone(const string& name, const string& OS, const int price) {
+		puts("Parmeter Construtor\n");
+	}
+	~Phone() {
+		cout << "this destector\n" << endl;
+	}
+};
+
+Phone::Phone() {
+	cout << "this is constructor\n " << endl;
+};
+Phone::Phone(const string& name, const string& OS, const int price) {
+	name =
+}
+
+
+int main() {
+	Phone Samsung();
+		
+}
