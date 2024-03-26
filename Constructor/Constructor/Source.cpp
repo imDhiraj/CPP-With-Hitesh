11064197dc8c4520958c17f8f8a6fa11ce496910
dhiraj <dhirajpk28@gmail.com>
2024-03-26 23:58:33 +0530

    rule of 3 for consterutre topic is done there are more thing but consture in nice

--- Constructor/Constructor/Source.cpp
+++ Constructor/Constructor/Source.cpp
@@ -15,14 +15,34 @@ public:
 	string getos() {
 		return _os;
 	}
-	//	~Phone() {puts("Distructure");}
+	Phone(const string& name, const string& os, const int& price);
+	Phone(const Phone&);
+	//~Phone() {puts("Distructure");}
 
 };/////////////////////////////////////////C++ return type may not be specified on a constructor///////////////This Semi colon is most inportant other wise class whlie show follwing error 
 Phone::Phone() : _name(), _os("oxy"), _price() {
 	puts("Defult Consturture");
 }
 
+Phone::Phone(const string& name, const string& os, const int& price) : _name(name), _os(os), _price(price) {
+	puts("prametraes constutacture");
+}
+
+Phone::Phone(const Phone& value)  {
+	puts("copy constrture");
+	_name = value._name;
+	_os = "Skined-"+value._os;
+	_price = value._price;
+}
+
 int main() {
 	Phone Samsung;
 	cout << Samsung.getos() << endl;
+
+	Phone Redmi("Redmi", "Android", 7999);
+	cout << Redmi.getos() << endl;
+
+	Phone Redmi7a = Redmi;
+	cout << Redmi7a.getos() << endl;
+
 }
