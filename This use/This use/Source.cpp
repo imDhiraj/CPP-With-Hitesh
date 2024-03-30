f047d092c8201f36947d65636851f8495fbeb416
dhiraj <dhirajpk28@gmail.com>
2024-03-30 19:47:32 +0530

    THIS - it is an key word for the used verious perposes it will used in frether code

---
+++ This use/This use/Source.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class Rectangle {
+	double _length ;
+	double _breadth ;
+
+public:
+	Rectangle(double l = 3.0, double b = 3.0) {
+		double _length = l;
+		double _breadth = b;
+	}
+	int area() {
+
+	}
+	int compare();
+};
