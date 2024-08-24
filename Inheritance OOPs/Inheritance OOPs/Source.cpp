788ca1414369982b004848dc7cda249c0050a5be
dhiraj <dhirajpk28@gmail.com>
2024-08-24 23:00:31 +0530

    USe of Friend class &its cons of use

--- Inheritance OOPs/Inheritance OOPs/Source.cpp
+++ Inheritance OOPs/Inheritance OOPs/Source.cpp
@@ -5,6 +5,11 @@ class Man {
 	string _name;
 	int _age;
 	Man() {}
+
+	//friend class
+	friend class superman;//=this giveis acees to class super man to acess all members from man like private members
+	//using frined class can be use full but it can cause sucrity issue some time besace of giveing enteire class function and not prticuler
+
 protected: // this is an BASE CLASS so we will try to define as much is tration function that will we need put in this 
 	Man(const string& name, const int& age) :_name(name), _age(age) {
 
@@ -12,6 +17,9 @@ protected: // this is an BASE CLASS so we will try to define as much is tration 
 	void Run() {
 		puts("i Can Run");
 
+	}
+	int getAge() {
+		return _age;//ALTERNAT toothe friend class ==  we can use this kind of function that can give acess of perticule meaber without exposeing all 
 	}
 public:
 	void sayMyname()const;
@@ -27,7 +35,8 @@ public:
 	superman(string name) : Man(name, 26) {
 	}
 	void Run() {
-		puts("i Can Run at light speed");
+		//puts("i Can Run at light speed");
+		printf("i can use the webs to sving and my age is %d\n", _age);
 	}
 
 };
@@ -39,7 +48,8 @@ public:
 	spiderman(string name) : Man(name, 19) {
 	}
 	void Run() {
-		puts("i Can Run at normal speed");
+		//puts("i Can Run at normal speed");
+		printf("ican use the webs to sving and my age is %d\n", getAge());
 	}
 
 };
