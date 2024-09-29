f8a75161d98ffd95432e5c3a3176747cc49fb653
dhiraj <dhirajpk28@gmail.com>
2024-09-29 16:27:31 +0530

    vecter method different aprochs

--- STL-Parition/STL-Parition/Source.cpp
+++ STL-Parition/STL-Parition/Source.cpp
@@ -16,17 +16,19 @@ int main() {
 	// 1 . partition 
 	//     it create paetition based on condtion we can use lambds function or unery function for it as will 
 
-	partition(begin(inty), end(inty), [](auto v) {return v % 2 == 0; });
+	partition(begin(inty), end(inty), [](auto v) {return v % 2 == 0; });//the begin and end can also be used by multiple method also these is the one
 	for (int x : inty) {
 		cout << x<<" ";
 	}
 	cout << endl;
 	// 2. stable partition 
 	// it whlie try to menten the partition in sorted order
-	stable_partition(inty.begin(), inty.end(), [](auto s) {return s / 2 == 0; });
+	stable_partition(inty.begin(), inty.end(), [](auto s) {return s / 2 == 0; });//the is the other method 
 	for (int x : inty) {
 		cout << x << " ";
 	}
 	// 3. partition point 
 	// this create an only partition point or only element that qulifues the condition
+	
+	}
 }
