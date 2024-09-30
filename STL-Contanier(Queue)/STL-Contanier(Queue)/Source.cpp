adb66697abf82cdee7f0d2928505870dba641613
Dhiraj Kumbhar <93609511+imDhiraj@users.noreply.github.com>
2024-09-30 06:45:01 +0530

    STL-Priority queue it is an sub type of queue which used priority based work like thaerad which need to be run bease of work quntity

--- STL-Contanier(Queue)/STL-Contanier(Queue)/Source.cpp
+++ STL-Contanier(Queue)/STL-Contanier(Queue)/Source.cpp
@@ -8,21 +8,44 @@ using namespace std;
 //
 
 int main() {
-	queue<int>myQueue;
+	/*queue<int>myQueue;
 	myQueue.push(10);
 	myQueue.push(11);
 	myQueue.push(12);
 	myQueue.push(13);
 	myQueue.push(14);
-	myQueue.push(15);
+	myQueue.push(15);*/
 
-	cout<<myQueue.front()<<endl;
-	cout << myQueue.back() << endl;
+	//cout << myQueue.front() << endl;
+	//cout << myQueue.back() << endl;
 
-	while (!myQueue.empty()) {
-		cout << " " << myQueue.front() << endl;// this loop is used iteating thorugh the entire queue by poping the element because queues are cant use intterate like for loop
-		myQueue.pop();
+	////// this loop is used iteating thorugh the entire queue by poping the element because queues are cant use intterate like for loop
+
+	//while (!myQueue.empty()) { 
+	//
+	//	cout << " " << myQueue.front() << endl;
+	//	myQueue.pop();
+	//}
+	////cout << myQueue.front() << endl;
+
+
+	priority_queue<int>mypq;
+
+	mypq.push(5);
+	mypq.push(30);
+	mypq.push(50);
+	mypq.push(10);
+	mypq.push(40);
+
+	while (!mypq.empty()) {
+		cout << " " << mypq.top(); 
+		/*this tis the method used for prority queue ut give more proiority to number that are big bit the queue element stays the same for indiue level but in combination the bgger the number it well get proirties
+		Output:
+		50 40 30 10 5
+			5*/
+		mypq.pop();
 	}
-	cout << myQueue.front() << endl;
+	cout << mypq.top() << endl;
+	// this is use in theding consecpt where proirty based sorting of work is need and proirity queue perform it very well
 
 }
