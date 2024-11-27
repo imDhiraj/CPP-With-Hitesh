8c4b82029417e0ff06ac8d697bd21929a592947c
Dhiraj Kumbhar <93609511+imDhiraj@users.noreply.github.com>
2024-11-28 00:46:49 +0530

    changes in BItwise Operations Expainations  on opreations that has main bit basedd expainations

--- bitwise optations/bitwise optations/Source.cpp
+++ bitwise optations/bitwise optations/Source.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 using namespace std;
+// This entrie calculation are  based on how many bit can data type care like int 32 bits , char 8 bits like this 
 
 int main() {
 	unsigned int a = 3;
 	unsigned int b = 7;
-
-	int c = a&b	;// AND
+	int c = ~a;
+	//int c = a&b	;// AND
 	/*  int c = a | b;   //OR
 	    int c = a << 1; //left shift
 		int c = a >> 1; //right shift
-		int c = ~ a;    //complement(not)
+		int c = ~ a;    //complement(not)  flip the vaules and retuning value is the answer 
+		int c = ^ a; // XOR           only 0^0 &&  1^1 = 0 and 0^1=1 [XOR TABLE]
 			*/
 	cout << c << endl;
 }
@@ -20,4 +22,5 @@ int main() {
 //and then perform given opertion on each indivial bool value of both for left to right
 
 //2.this for NOT
-//in this bitwise opratoer while flep the bits for orginal and to make it equal to orginal value add nagtive to value 
+//in this bitwise opratoer while flep the bits for orginal and to make it equal to orginal value add nagtive to value  
+// read about htis this has answer why this not (~) of c is giving -4 in answer https://chatgpt.com/share/67476c3f-2008-8009-92ab-146c7a4d1e0b
